e2c75cb39719f7270d0170ef0970d91a3b84f289
Eudes Costa <ecosta@redhat.com>
2021-11-14 19:11:16 +0100

    simple http server

--- server.py
+++ server.py
@@ -7,4 +7,4 @@ def main():
     return ('ok', 200)
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True, port=5000)
+    app.run(host="0.0.0.0", debug=True, use_reloader=True, port=5000)
