1186ea956eb975029fe33c3bc3fed1ba4229fc90
Eudes Costa <ecosta@redhat.com>
2021-11-15 17:58:58 +0100

    simple http server, wip

--- server.py
+++ server.py
@@ -1,18 +1,16 @@
 from flask import Flask, request
-import requests
+import requests, json
+
+import logging; log = logging.getLogger(__name__)
+DEBUG=log.debug; INFO=log.info; WARN=log.warning; ERROR=log.error
 
 app = Flask(__name__)
 
 SECRET = "some-secret" # this is a test, the real one will be on ocp secrets...
-EVENT_NEW_IMAGE = "new-image"
+# EVENT_NEW_IMAGE = "new-image"
+# EVENT_LISTENER = "http://el-event-listener-nj389f-rht-webapp-argoqa.apps.ocp-delta.ocp-delta.ole.redhat.com/"
 
-# expected payload
-# {
-#     "event" : "new-image",
-#     "eventid" : "1234",
-#     "namespace" : "rht-webapp-devel",
-#     "secret" : "..."
-# }
+loaded_configurations = None
 
 @app.route('/build-notification', methods=['POST'])
 def build_notification():
@@ -23,9 +21,9 @@ def build_notification():
     secret = request_data['secret']
     if (secret != SECRET):
         return ('not authorized', 404)
-    
+
     # if payload is not as expected
-    if not _process_incoming_data(request_data):
+    if not _process_incoming_data(request_data, request.path):
         return ("error processing incoming data", 500)
 
     # processing
@@ -35,18 +33,38 @@ def build_notification():
 
     return ("ok", 200) 
 
-def _process_incoming_data(request_data):
-    try:
-        event = request_data['event']
-        eventid = request_data['eventid']
-        namespace = request_data['namespace']
-        # ...
-    except(KeyError):
-        return False
-    return True
+def _load_manifest():
+    """
+    Loads manifest file
+    """
+    with open('manifest.json') as json_file:
+        return json.load(json_file)
+
+def _load_attr_fqn(fqn):
+    """
+    Loads mandatory attributes for FQN from manifest file
+    """
+    for n in loaded_configurations["routes"]:
+        if n["fqn"] == fqn:
+            return n["attributes"]
+
+def _process_incoming_data(request_data, fqn):
+    """
+    Sanity check if expected keys defined for the requested FQN are there based on the 
+        manifest file
+    """
+    expected_attr = _load_attr_fqn(fqn)
+    a = []
+    for k in request_data.keys():
+        a.append(k)
+    if all (k in a for k in expected_attr):
+        return True
+    return False
 
 def _process_new_image_event():
-    return requests.get('http://example.com').content
+    payload = '{"eventid" : "1234", "event" : "new-image", "namespace" : "rht-webapp-devel"}'
+    return requests.post('http://example.com', json=payload)
 
 if __name__ == '__main__':
+    loaded_configurations = _load_manifest()
     app.run(host="0.0.0.0", debug=True, use_reloader=True, port=5000)
